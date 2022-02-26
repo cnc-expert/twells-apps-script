4db6c2b0b9f1c8d961a1c83a71a689d7cb9977be
cnc-expert <cnc-expert@mail.ru>
2022-02-26 09:10:37 +0000

    Feat: parsing of 114C. 	modified:   pkg/parser.js 	modified:   pkg/parser.test.js

--- pkg/parser.js
+++ pkg/parser.js
@@ -1,66 +1,74 @@
 const ThermowellModels = {
 	model114C: "114C",
-	modelD01:  "D01",
+	modelD01: "D01",
 	model0096: "0096",
 	modelUnknown: ""
 }
 
-/*
- * Detects thermowell model
- * @param  {string}  code  An order code.
- * @return {string}  The order code 114C.
- */
-function detectModel(code) {
-	switch (true) {
-		case !!code.match(/^\s*114/i):
-			return ThermowellModels.model114C;
-		case !!code.match(/^\s*D0*1/i):
-			return ThermowellModels.modelD01;
-		case !!code.match(/^\s*0*96/i):
-			return ThermowellModels.model0096;
-		default:
-			return ThermowellModels.modelUnknown;
+class ThermowellParser {
+
+	/*
+	 * Detects thermowell model.
+	 *
+	 * @param  {string}  code  An order code.
+	 * @return {string}  The order code 114C.
+	 */
+	static detectModel(code) {
+		switch (true) {
+			case !!code.match(/^\s*114/i):
+				return ThermowellModels.model114C;
+			case !!code.match(/^\s*D0*1/i):
+				return ThermowellModels.modelD01;
+			case !!code.match(/^\s*0*96/i):
+				return ThermowellModels.model0096;
+			default:
+				return ThermowellModels.modelUnknown;
+		}
 	}
-}
 
-class RosemountTWells {
+	/**
+	 * Parse an option.
+	 * 
+	 * @param  {string}  opt  An option.
+	 * @return {object}  An object with keys "sym" and "num".
+	 */
+	static parseOption(optCode) {
+		const matched = optCode.match(/(?<sym>[A-Z]*)(?<num>\d*)/i);
+		return {
+			sym: matched?.groups.sym.toUpperCase(),
+			num: matched?.groups.num | 0
+		};
+	}
 
-  /**
-   * Parse options string
-   * @param  {string}  opts  List of options from thermowell order string
-   * @return {string[]}      List of parsed options
-   */
-  static parseOptions(opts) {
-    const reOptions = /[A-Z]{1,2}\d*/ig;
-    return opts?.match(reOptions) ?? [];
-  }
+	/**
+	 * Parse options string.
+	 * 
+	 * @param  {string}  optString  List of options from thermowell order string.
+	 * @return {string[]}  List of parsed options.
+	 */
+	static parseOptions(optString) {
+		const reOptions = /[A-Z]{1,2}\d*/ig;
+		const strOptions = optString?.match(reOptions) ?? [];
+		return strOptions.map(ThermowellParser.parseOption);
+	}
 
-  /**
-   * Parse 114C code order
-   * @param  {string}  code  Order code of the thermowell
-   * @return {object}        Parsed parameters
-   */
-  static C114_parse(code) {
-    const reMain = new RegExp("114C *(?<unit>.) *(?<immerLen>....) *(?<mntTyp>.) *(?<procConn>..) *(?<stemStyle>.) *(?<mater>..) *(?<headLen>...) *(?<instrCon>.) *(?<opts>.*)", "i");
-    const mainParams = reMain.exec(code);
-    const optionsList = parseOptions_(mainParams?.groups.opts);
-    return { ...mainParams?.groups, optionsList };
-  }
-
-  /**
-   * Human readable values of parsed code
-   * @param  {string}  code  Order code of the thermowell
-   * @return {object}        Parsed parameters
-   */
-  static C114_decode(code) {
-    const params = t114C_parse_(code)
-    // TO-DO
-    return params;
-  }
-
+	/**
+	 * Parse 114C code order.
+	 * 
+	 * @param  {string}  code  Order code of the thermowell.
+	 * @return {object}        Parsed parameters or null if cannot parse.
+	 */
+	static parse114C(code) {
+		const reMain = /(?<model>114C) *(?<unit>.) *(?<immerLen>....) *(?<mntTyp>.) *(?<procConn>..) *(?<stemStyle>.) *(?<mater>..) *(?<headLen>...) *(?<instrCon>.) *(?<opts>.*)/i;
+		const mainParams = code.match(reMain);
+		if (!mainParams) return null;
+		const optionsList = ThermowellParser.parseOptions(mainParams.groups.opts);
+		delete mainParams.groups.opts;
+		return { ...mainParams?.groups, optionsList };
+	}
 }
 
 module.exports = {
 	ThermowellModels,
-	detectModel
+	ThermowellParser
 }
--- pkg/parser.test.js
+++ pkg/parser.test.js
@@ -1,40 +1,116 @@
-const parser = require("./parser.js")
+const parser = require("./parser.js");
 
 describe("Detection of model", () => {
+
 	test("D01", () => {
 		const input = "D01D5CT46ADN000";
 		const want = parser.ThermowellModels.modelD01;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
+	});
+
 	test("114C", () => {
 		const input = "  114CM0355VAR1SC100AWR5C02Q5R16R60TR \ Re";
 		const want = parser.ThermowellModels.model114C;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
+	});
+
 	test("0096", () => {
 		const input = "096-Y-0350-T98";
 		const want = parser.ThermowellModels.model0096;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
+	});
+
 	test("bad D01", () => {
 		const input = "thermowell D01D5CT46ADN000";
 		const want = parser.ThermowellModels.modelUnknown;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
+	});
+
 	test("bad 114C", () => {
 		const input = "ll4CM0355VAR1SC100AWR5C02Q5R16R60TR \ Re";
 		const want = parser.ThermowellModels.modelUnknown;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
+	});
+	
 	test("bad 0096", () => {
 		const input = "009b-Y-0350-T98";
 		const want = parser.ThermowellModels.modelUnknown;
-		const got = parser.detectModel(input);
+		const got = parser.ThermowellParser.detectModel(input);
 		expect(got).toBe(want);
-	})
-})
+	});
+});
+
+
+
+describe("Parsing option list", () => {
+
+	test("Options of 114C and 0096", () => {
+		const input = " , Q17Q5Q73 R06TRR01R05, R07m523";
+		const want = [
+			{ sym: "Q", num: 17 },
+			{ sym: "Q", num: 5},
+			{ sym: "Q", num: 73},
+			{ sym: "R", num: 6},
+			{ sym: "TR", num: 0},
+			{ sym: "R", num: 1},
+			{ sym: "R", num: 5},
+			{ sym: "R", num: 7},
+			{ sym: "M", num: 523},
+		]
+		const got = parser.ThermowellParser.parseOptions(input);
+		expect(got).toEqual(want);
+	});
+});
+
+
+
+describe("Parsing 114C", () => {
+
+	test("Parsing 114C without options", () => {
+		const input = "114CM0355VAR1SC100A";
+		const got = parser.ThermowellParser.parse114C(input)
+		const want = {
+			model: '114C',
+			unit: 'M',
+			immerLen: '0355',
+			mntTyp: 'V',
+			procConn: 'AR',
+			stemStyle: '1',
+			mater: 'SC',
+			headLen: '100',
+			instrCon: 'A',
+			optionsList: []
+		};
+		expect(got).toEqual(want);
+	});
+
+	test("Parsing 114C with options", () => {
+		const input = "114CM0355VAR1SC100AWR5C02Q5R16R60TR \ ";
+		const got = parser.ThermowellParser.parse114C(input)
+		const want = {
+			model: '114C',
+			unit: 'M',
+			immerLen: '0355',
+			mntTyp: 'V',
+			procConn: 'AR',
+			stemStyle: '1',
+			mater: 'SC',
+			headLen: '100',
+			instrCon: 'A',
+			optionsList: [
+				{ sym: 'WR', num: 5 },
+				{ sym: 'C', num: 2 },
+				{ sym: 'Q', num: 5 },
+				{ sym: 'R', num: 16 },
+				{ sym: 'R', num: 60 },
+				{ sym: 'TR', num: 0 }
+			]
+		};
+		expect(got).toEqual(want);
+	});
+});
